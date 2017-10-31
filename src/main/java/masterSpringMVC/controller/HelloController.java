5758af1ae9d24c595e437072e202031e430d7db4
vu kieu anh <vu.kieu.anh@nextop.asia>
2017-10-31 17:05:27 +0700

    update code chapter 2

--- src/main/java/masterSpringMVC/controller/HelloController.java
+++ src/main/java/masterSpringMVC/controller/HelloController.java
@@ -13,15 +13,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-	@Autowired
-	private Twitter twitter;
-
-	@RequestMapping("/")
-	public String hello(@RequestParam(defaultValue = "masterSpringMVC4") String search, Model model) {
-		SearchResults searchResults = twitter.searchOperations().search(search);
-		List<Tweet> tweets = searchResults.getTweets();
-		model.addAttribute("tweets", tweets);
-		model.addAttribute("search", search);
-		return "resultPage";
-	}
+	
 }
