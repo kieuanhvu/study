be3130fd7499ab7f2cc20e9bdb5ef81d5be32152
vu kieu anh <vu.kieu.anh@nextop.asia>
2017-11-02 17:01:45 +0700

    Chapter 4: Translating the error messages

--- src/main/java/masterSpringMVC/MasterSpringMvcApplication.java
+++ src/main/java/masterSpringMVC/MasterSpringMvcApplication.java
@@ -2,8 +2,12 @@ package masterSpringMVC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import masterSpringMVC.config.PicturesUploadProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({PicturesUploadProperties.class})
 public class MasterSpringMvcApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/masterSpringMVC/config/PicturesUploadProperties.java
@@ -0,0 +1,23 @@
+package masterSpringMVC.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.Resource;
+
+@ConfigurationProperties(prefix = "upload.pictures")
+public class PicturesUploadProperties {
+	private Resource uploadPath;
+	private Resource anonymousPicture;
+	
+	public Resource getUploadPath() {
+		return uploadPath;
+	}
+	public void setUploadPath(Resource uploadPath) {
+		this.uploadPath = uploadPath;
+	}
+	public Resource getAnonymousPicture() {
+		return anonymousPicture;
+	}
+	public void setAnonymousPicture(Resource anonymousPicture) {
+		this.anonymousPicture = anonymousPicture;
+	}
+}
