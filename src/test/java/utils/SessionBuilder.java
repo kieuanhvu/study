6162e376c3352ddcc6e585e3a73817df6f45f900
kieuanhvu <kieuanhvu91@gmail.com>
2017-11-12 11:29:41 +0700

    Chapter 7: Mocks and stubs -> Mocking with Mockito

---
+++ src/test/java/utils/SessionBuilder.java
@@ -0,0 +1,27 @@
+package utils;
+
+import org.springframework.mock.web.MockHttpSession;
+
+import masterSpringMVC.profile.UserProfileSession;
+
+import java.util.Arrays;
+
+public class SessionBuilder {
+	private final MockHttpSession session;
+	UserProfileSession sessionBean;
+
+	public SessionBuilder() {
+		session = new MockHttpSession();
+		sessionBean = new UserProfileSession();
+		session.setAttribute("scopedTarget.userProfileSession", sessionBean);
+	}
+
+	public SessionBuilder userTastes(String... tastes) {
+		sessionBean.setTastes(Arrays.asList(tastes));
+		return this;
+	}
+
+	public MockHttpSession build() {
+		return session;
+	}
+}
